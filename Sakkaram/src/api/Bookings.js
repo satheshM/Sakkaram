f8e6b908ec4353b9b6622997e11607bd5b89f69e
SATHESH M <70007631+satheshM@users.noreply.github.com>
2025-03-10 10:36:28 +0530

    file uploaded

---
+++ Sakkaram/src/api/Bookings.js
@@ -0,0 +1,14 @@
+import { request } from './auth';
+
+export const createBooking = async (bookingData) =>
+  request('/bookings', 'POST', bookingData);
+
+export const GetUserBookings = async () => request('/user/bookings', 'GET');
+export const GetOwnerBookings = async () => request('/owner/bookings', 'GET');
+export const UpdateBookingStatus = async (status) =>
+  request(`/owner/bookings/status`, 'PATCH',status);
+
+  export const submitReview = async (review) =>
+  request(`/bookings/submitReview`, 'PATCH',review);
+
+
